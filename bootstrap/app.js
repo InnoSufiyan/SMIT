5f7dd9d6788805b3540bcf9a61f3a5cf27c04f25
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-02-01 10:19:22 +0500

    JS

---
+++ bootstrap/app.js
@@ -0,0 +1,15 @@
+var name = "sufiyan"
+alert(name)
+
+var age = 31;
+alert(age)
+
+var ageIncrementBySomeone = 9;
+alert(ageIncrementBySomeone)
+
+var incrementedAge = age + ageIncrementBySomeone
+alert(incrementedAge)
+
+var jd = "Mern Developer"
+alert(jd)
+
---
+++ javascript/app.js
@@ -0,0 +1,32 @@
+// var firstName = prompt("What is your first name??")
+// var lastName = prompt("What is your last name??")
+var day = prompt("Which day's menu , you want to know??")
+
+if (day == "monday") {
+    alert("murgi ki karahi")
+} else if (day == "tuesday") {
+    alert("sabzi")
+} else if (day == "wednesday") {
+    alert("sabzi")
+} else if (day == "thursday") {
+    alert("murgi")
+} else if (day == "friday") {
+    alert("biryani")
+} else if (day == "saturday") {
+    alert("daal chawal")
+} else {
+    alert("holiday")
+}
+
+
+
+
+// =
+// == equal comparison
+// === equal in value and date type
+// != not equal too
+// !== not equal too and no equal data type
+// >  greater than
+// <  smaller than
+// >=  greater than or equal too
+// <=  smaller than or equal too
