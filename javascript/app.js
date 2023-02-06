64c9b77978262ef76ae282f3910723a2e693c45a
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-02-06 10:31:33 +0500

    operators and if and else

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,14 @@
-var firstName = prompt("What is your first name??")
-var lastName = prompt("What is your last name??")
+// var firstName = prompt("What is your first name??")
+// var lastName = prompt("What is your last name??")
 
-var age = prompt("What is your age??")
+// var age = prompt("What is your age??")
 
 
 
-console.log("My name is " + firstName + " " + lastName + " and your current age is " + age + " and after 5 years you will be " + (Number(age) + 5))   //ES5
+// console.log("My name is " + firstName + " " + lastName + " and your current age is " + age + " and after 5 years you will be " + (Number(age) + 5))   //ES5
 
 // template literals       ES6
-console.log(`My name is ${firstName} ${lastName} and your current age is ${age} and after 5 years you will be ${Number(age) + 5}`)
+// console.log(`My name is ${firstName} ${lastName} and your current age is ${age} and after 5 years you will be ${Number(age) + 5}`)
 
 // data type
 
@@ -18,11 +18,71 @@ console.log(`My name is ${firstName} ${lastName} and your current age is ${age} 
 // undefined
 // null
 
-var student;
+// var student;
 
-console.log(student)
-console.log(student1)
+// console.log(student)
+// console.log(student1)
 
+// var age = 32;
+// var ageFromUser = +prompt("Type your age")
+
+// console.log(age)
+// console.log(ageFromUser)
+// console.log(typeof age)
+// console.log(typeof ageFromUser)
+
+// var doodhKiDukan = false
+
+// if (doodhKiDukan) {
+//     buy = "doodh le kar aajaoun";
+//     console.log(buy)
+// } else {
+//     buy = "na le kar aaoun";
+//     console.log(buy)
+// }
+
+// var money;
+// console.log(money, "money consoled with no value")
+
+
+// if (money) {
+//     console.log("aao shopping k liye chalain")
+// } else {
+//     console.log("chup chaap ghar main bethey raho")
+// }
+
+// money = 1000;
+// console.log(money, "after initializing value in money variable")
+
+// var age = prompt("type your age")
+
+// var isBlind = true
+// var isHelmet = true
+// var isLicense = true
+// var parchi = false
+
+//true           //true       //true        //true
+// if ((age >= 18 && !isBlind && isHelmet && isLicense) || parchi) {
+//     console.log("bike chalana allowed hai")
+// } else {
+//     console.log("aap ka kat jaeyga")
+// }
+
+//team A = 50, 20, 15, 3
+// team B = 20, 50, 9, 3
+
+var teamA = 180;
+var teamB = 50;
+
+if (teamA > teamB && teamA > 100) {
+    console.log("teamA won the match with marks " + teamA)
+} else if (teamA == teamB && teamA > 100 && teamB > 100) {
+    console.log("Match draw because both have same marks " + teamA)
+} else if (!(teamA > 100) && !(teamB > 100)) {
+    console.log("both teams lost the match")
+} else {
+    console.log("teamB won the match with marks " + teamB)
+}
 
 
 
