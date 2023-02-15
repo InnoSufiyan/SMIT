c10e46068c3b6d2349d7b003939f17fc858829ef
innosufiyan <innosufiyan@gmail.com>
2023-02-15 10:19:53 +0500

    array

---
+++ javascript/array/app.js
@@ -0,0 +1,12 @@
+var student1 = "Muhammad Naif"
+var student2 = "Muhammad Mustafa"
+var student3 = "Aamir"
+var teacher = "sufiyan"
+
+// undefined, null, string, number, boolean, symbol, bigInt ==>> primitive datatypes
+
+// var multiValues = ["Muhammad Naif", "Muhammad Mustafa", "Aamir", true, 365, "asdasdasda", teacher]
+var students = ["Muhammad Naif", "Muhammad Mustafa", "Aamir"]
+
+alert(students[students.length - 1])
+
--- javascript/functions/app2.js
+++ javascript/functions/app2.js
@@ -26,33 +26,82 @@
 // console.log(c)
 
 
-var userEmail = "sufiyan@gmail.com"
-var userPassword = 12345
+// var userEmail = "sufiyan@gmail.com"
+// var userPassword = 12345
 
-function userValidation() {
-    var userInputEmail = prompt("Please write your email address")
-    var userInputPassword = +prompt("Please write your Password")
+// function userValidation() {
+//     var userInputEmail = prompt("Please write your email address")
+//     var userInputPassword = +prompt("Please write your Password")
 
-    if (userEmail === userInputEmail && userPassword === userInputPassword) {
-        return true
-    } else if (userEmail !== userInputEmail) {
-        alert("sorry your email is incorrect")
-        return false
-    } else if (userPassword !== userInputPassword) {
-        alert("sorry your password is incorrect")
-        return false
-    } else {
-        alert("sorry na password sahi, na email")
-        return false
-    }
-}
+//     if (userEmail === userInputEmail && userPassword === userInputPassword) {
+//         return true
+//     } else if (userEmail !== userInputEmail) {
+//         alert("sorry your email is incorrect")
+//         return false
+//     } else if (userPassword !== userInputPassword) {
+//         alert("sorry your password is incorrect")
+//         return false
+//     } else {
+//         alert("sorry na password sahi, na email")
+//         return false
+//     }
+// }
 
 
-function login() {
-    if (userValidation()) {
-        alert("moving you to home page")
-    } else {
-        alert("har jagah false aaya")
-    }
-}
+// function login() {
+//     if (userValidation()) {
+//         alert("moving you to home page")
+//     } else {
+//         alert("har jagah false aaya")
+//     }
+// }
 
+//function declaration
+
+myFunction()
+myFunction()
+
+// function myFunction() {
+//     console.log("This is our function declaration")
+// }
+
+// const meraEs6Expression = function () {
+//     //logic
+// }
+// const meraEs6Expression2 = () => {
+//     //logic
+// }
+// const meraEs6Expression3 = () => "logic"
+
+
+
+
+
+
+
+
+
+
+
+//function expression
+
+// myFunction2()
+
+// const myFunction2 = function () {
+
+// }
+
+
+// const myFunction2 = () => "dasdsada"
+
+// myFunction2()
+
+//arrow function
+
+// const myFunction3 = (name, age) => {
+//     return `My name is ${name} and my age is ${age}`
+//     // return "My name is " + name + " and my age is " + age
+// }
+// const myFunction3 = (name, age) => `My name is ${name} and my age is ${age}`
+
+// myFunction3("sufiyan", 32)
