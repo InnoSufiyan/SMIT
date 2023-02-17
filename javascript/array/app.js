e8dabb86c30062848b2e0cedc7db1610e5694367
innosufiyan <innosufiyan@gmail.com>
2023-02-17 10:22:29 +0500

    assignments and splice and slice

--- javascript/array/app.js
+++ javascript/array/app.js
@@ -3,10 +3,46 @@ var student2 = "Muhammad Mustafa"
 var student3 = "Aamir"
 var teacher = "sufiyan"
 
+
+// var cricketers = []
+
+// console.log(cricketers[3])
+
+// console.log(cricketers)
+
+// cricketers[0] = "Babar azam"
+
+// console.log(cricketers)
+
+// cricketers[1] = "waseem akram"
+
+// console.log(cricketers)
+
+// cricketers[6] = "Shaheen Afridi"
+
+// console.log(cricketers)
+
 // undefined, null, string, number, boolean, symbol, bigInt ==>> primitive datatypes
 
 // var multiValues = ["Muhammad Naif", "Muhammad Mustafa", "Aamir", true, 365, "asdasdasda", teacher]
-var students = ["Muhammad Naif", "Muhammad Mustafa", "Aamir"]
+var students = ["Muhammad Naif", "Muhammad Mustafa", "Aamir", "Ibad"]
 
-alert(students[students.length - 1])
+console.log(students)
+
+var studentsWithJamai = students.splice(2, 2)
+
+console.log(studentsWithJamai)
+console.log(students)
+//Adding in Start
+// var checkingAdditionInStart = students.unshift("Muhammad Javed")
+
+// Removing from start
+// var checkingRemovingFromStart = students.shift()
+
+// Adding in end
+// const additionFromEnd = students.push("Ibad")
+
+// Removing from end
+// const removingFromEnd = students.pop()
+
 
