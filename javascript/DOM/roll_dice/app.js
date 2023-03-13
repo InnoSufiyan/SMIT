64eab07ce2547b7a41f9b732493fd8bdf9ebe7f5
innosufiyan <innosufiyan@gmail.com>
2023-03-13 10:15:55 +0500

    roll dice

--- javascript/DOM/roll_dice/app.js
+++ javascript/DOM/roll_dice/app.js
@@ -4,12 +4,23 @@ var player2Score = document.querySelector('.player2Score')
 var player1DiceScore = document.querySelector('.player1DiceScore')
 var player2DiceScore = document.querySelector('.player2DiceScore')
 
+
+var player1PreviousScore = document.querySelector('.player1PreviousScore')
+var player2PreviousScore = document.querySelector('.player2PreviousScore')
+
+var player1 = document.querySelector('.player1')
+var player2 = document.querySelector('.player2')
+
 var newGameBtn = document.querySelector('.newGameBtn')
 var diceImg = document.querySelector('.diceImg')
 var rollDiceBtn = document.querySelector('.rollDiceBtn')
 var holdBtn = document.querySelector('.holdBtn')
 
 rollDiceBtn.addEventListener('click', RollDiceHandler)
+
+var currentPlayer = 1
+
+var previousScore = [0, 0]
 
 function RollDiceHandler() {
     // var diceNumber = Math.round(Math.random() * 6)
@@ -18,9 +29,28 @@ function RollDiceHandler() {
     // console.log(diceImg.src)
     diceImg.src = `./assets/${diceNumber}.png`
     if (diceNumber == 1) {
-        player1DiceScore.textContent = 0;
+        if (currentPlayer == 1) {
+
+            player1PreviousScore.textContent = Number(player1DiceScore.textContent)
+
+            player1DiceScore.textContent = 0;
+            currentPlayer = 2
+        } else {
+            currentPlayer = 1
+
+
+            player2PreviousScore.textContent = Number(player2DiceScore.textContent)
+
+            player2DiceScore.textContent = 0;
+        }
+        player1.classList.toggle('active')
+        player2.classList.toggle('active')
     } else {
-        player1DiceScore.textContent = Number(player1DiceScore.textContent) + diceNumber
+        if (currentPlayer == 1) {
+            player1DiceScore.textContent = Number(player1DiceScore.textContent) + diceNumber
+        } else {
+            player2DiceScore.textContent = Number(player2DiceScore.textContent) + diceNumber
+        }
     }
 }
 
@@ -28,11 +58,35 @@ holdBtn.addEventListener('click', holdHandler)
 
 
 function holdHandler() {
-    player1Score.textContent = Number(player1Score.textContent) + Number(player1DiceScore.textContent)
 
-    player1DiceScore.textContent = 0
+    player1.classList.toggle('active')
+    player2.classList.toggle('active')
 
-    if(Number(player1Score.textContent) >= 30) {
-        alert("Player 1 wins")
+
+    if (currentPlayer == 1) {
+        currentPlayer = 2
+        player1Score.textContent = Number(player1Score.textContent) + Number(player1DiceScore.textContent)
+        player1PreviousScore.textContent = Number(player1DiceScore.textContent)
+
+        player1DiceScore.textContent = 0
+        if (Number(player1Score.textContent) >= 30) {
+            alert("Player 1 wins")
+        }
+
+
+    } else {
+        currentPlayer = 1
+        player2Score.textContent = Number(player2Score.textContent) + Number(player2DiceScore.textContent)
+
+
+        player2PreviousScore.textContent = Number(player2DiceScore.textContent)
+
+        player2DiceScore.textContent = 0
+        if (Number(player2Score.textContent) >= 30) {
+            alert("Player 2 wins")
+        }
     }
+
+    console.log(currentPlayer, "==>>currentPlayer")
+
 }
