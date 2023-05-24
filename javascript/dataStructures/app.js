3f31bac00ff3d2386617b2e5ac90f70da6ccfbac
innosufiyan <innosufiyan@gmail.com>
2023-05-24 10:06:34 +0500

    destructurization

---
+++ javascript/dataStructures/app.js
@@ -0,0 +1,55 @@
+// var students = ["zakir", "hamza", "hammad", ["reyan", "adnan"]]
+
+// var student1 = students[0]
+// var student2 = students[1]
+// var student3 = students[2]
+// var student4 = students[3]
+
+// var [std1, , std2] = students
+
+// console.log(std1)
+// console.log(std2)
+// console.log(std3)
+
+
+// var tournaments = {
+//     name: "tournament 1",
+//     game: "CSGO",
+//     teams: 25,
+//     teamMembers: 5,
+//     prizeMoney: "500$",
+//     entryFees: "5$"
+// }
+
+// var name = tournaments.name
+// var game = tournaments.game
+// var teams = tournaments.teams
+// var teamMembers = tournaments.teamMembers
+// var prizeMoney = tournaments.prizeMoney
+
+// var {prizeMoney : money, entryFees, name, game, teamMembers, teams} = tournaments
+
+// console.log(money)
+
+// var a = 15
+// var b = 25
+
+// var c = a
+// a = b
+// b = c
+
+
+// console.log(a)
+// console.log(b)
+          //15,25
+        //   [15,25]
+// var [b,a] = [a,b]
+
+// console.log(a)
+// console.log(b)
+
+function foo(a) {
+    return 15 - a
+} 
+
+console.log(foo(5))
