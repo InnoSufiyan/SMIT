61b58323eaf3707c0321d851789ade54bde60490
innosufiyan <innosufiyan@gmail.com>
2023-05-29 10:20:40 +0500

    social media app login functionality

--- javascript/DOM/socialMediaApp/app.js
+++ javascript/DOM/socialMediaApp/app.js
@@ -1,9 +1,10 @@
-const email = document.querySelector('#email')
+const loginEmail = document.querySelector('#email')
 const loginPassword = document.querySelector('#inputPassword')
 const firstName = document.querySelector('#recipient-name')
 const surName = document.querySelector('#recipient-surname')
 const mobNum = document.querySelector('#recipient-mobile')
 const password = document.querySelector('#recipient-pass')
+const email = document.querySelector('#recipient-email')
 
 
 const loginBtn = document.querySelector('#login')
@@ -23,33 +24,95 @@ loginBtn.addEventListener('click', loginHandler)
 signupBtn.addEventListener('click', signupHandler)
 
 function loginHandler() {
-    console.log(email.value)
+    console.log(loginEmail.value)
     console.log(loginPassword.value)
+
+    if(!loginEmail.value || !loginPassword.value) return alert("Please write email and password to continue")
+
+    const userFound = users.filter((user) => {
+        console.log("user email in userFound filter", user.email)
+        return user.email === loginEmail.value
+    })
+
+    console.log(userFound, "==>>user found in login")
+
+    if(!userFound.length) return alert("This user is not registered, kindly create an account first")
+
+
+    console.log(loginPassword.value, "===>>> login password of input")
+    console.log(userFound.password, "user password from local storage")
+
+    if(userFound[0].password == loginPassword.value) {
+        alert("user is logging in")
+        
+        localStorage.setItem('isLoggedInUser', JSON.stringify(userFound[0]))
+
+
+        window.location.href="./dashboard/index.html";
+
+
+    } else {
+        alert("password is incorrect")
+    }
 }
 
 
 
 function signupHandler() {
+
+    console.log("user email in our input", email.value)
+
+    const userFound = users.filter((user) => {
+        console.log("user email in userFound filter", user.email)
+        return user.email === email.value
+    })
+
+    if (userFound.length) return alert("Email address already in use, please use another email address")
+
+    // mobile number validation
+
+
+    console.log("user mil gaya ==>>>", userFound)
+
+    const modal = document.getElementById('exampleModal')
+
+    console.log(modal)
+
+    modal.classList.toggle('show')
+
+
+
     if (firstName.value !== "" && surName.value !== "" && mobNum.value !== "" && password.value !== "" && date !== undefined && month !== undefined && year !== undefined && gender !== undefined) {
-        if (password.value.length < 8) return alert("password should contain 8 characters")
+        // if (password.value.length < 8) return alert("password should contain 8 characters")
 
-        const userObj = {
-            firstName: firstName.value,
-            surName: surName.value,
-            mobileNum: mobNum.value,
-            password: password.value,
-            date: new Date(`${year}-${month}-${date}`),
-            // gender: gender
-            gender
-        }
+        // const userObj = {
+        //     firstName: firstName.value,
+        //     surName: surName.value,
+        //     mobileNum: mobNum.value,
+        //     password: password.value,
+        //     date: new Date(`${year}-${month}-${date}`),
+        //     email: email.value,
+        //     // gender: gender
+        //     gender
+        // }
 
-        
 
-        users.push(userObj)
 
-        localStorage.setItem('users', JSON.stringify(users))
+        // users.push(userObj)
 
-        alert("User signup successfully")
+        // localStorage.setItem('users', JSON.stringify(users))
+
+        // alert("User signup successfully")
+
+        // firstName.value = ""
+        // surName.value = ""
+        // mobNum.value = ""
+        // password.value = ""
+        // email.value = ""
+
+
+
+
 
     } else {
         alert("insaan ban k saari fields fill up karlo")
