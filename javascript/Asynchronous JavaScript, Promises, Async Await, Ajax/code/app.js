d32b9bf2241b9f91bdc4d693126cc85797dd0835
InnoSufiyan <innosufiyan@gmail.com>
2023-06-23 10:53:22 +0500

    fetch

--- javascript/Asynchronous JavaScript, Promises, Async Await, Ajax/code/app.js
+++ javascript/Asynchronous JavaScript, Promises, Async Await, Ajax/code/app.js
@@ -1,47 +1,115 @@
-const uiCreation = ({title, image, description}) => {
-    // console.log(data, "==>> uiCreation function")
+// const uiCreation = ({title, image, description}) => {
+//     // console.log(data, "==>> uiCreation function")
 
-    // const {title, image, description} = data
+//     // const {title, image, description} = data
 
-    console.log(title, image, description, "===>> destructure")
+//     console.log(title, image, description, "===>> destructure")
+
+//     const imageUi = document.querySelector('#image')
+//     const cardTitle = document.querySelector('.card-title')
+//     const cardText = document.querySelector('.card-text')
+
+
+
+
+//     console.log(cardTitle, "==>>cardTitle in uiCreation")
+//     console.log(imageUi, "==>>image in uiCreation")
+//     // imageUi.src = data.image
+//     imageUi.src = image
+//     // cardTitle.textContent = data.title
+//     cardTitle.textContent = title
+//     // cardText.textContent = data.description
+//     cardText.textContent = description
+
+// }
+
+
+// const getCountryData = (num) => {
+//     fetch(`https://fakestoreapi.com/products/${num}`)
+//         .then(function (response) {
+//             console.log(response, "==>> response of fetch")
+//             return response.json()
+//         }).then(function(data){
+//             console.log(data, "==>> my data as json")
+//             uiCreation(data)
+//         })
+// }
+
+// getCountryData(1)
+
+
+// const getAllProducts = () => {
+//     fetch(`https://fakestoreapi.com/products/`)
+//         .then(resp => resp.json())
+//         .then(data => console.log(data, "==>>allProducts"))
+// }
+
+// getAllProducts()
+
+const uiCreation = (countryData) => {
+
+    console.log(countryData)
 
     const imageUi = document.querySelector('#image')
     const cardTitle = document.querySelector('.card-title')
     const cardText = document.querySelector('.card-text')
 
-
-
-
-    console.log(cardTitle, "==>>cardTitle in uiCreation")
-    console.log(imageUi, "==>>image in uiCreation")
     // imageUi.src = data.image
-    imageUi.src = image
+    imageUi.src = countryData.flags.png
     // cardTitle.textContent = data.title
-    cardTitle.textContent = title
+    cardTitle.textContent = countryData.name.common
     // cardText.textContent = data.description
-    cardText.textContent = description
+    cardText.textContent = `languages in this country commonly used are ${countryData.languages.eng} and ${countryData.languages.urd}`
 
 }
 
+const displayError = (err) => {
+    const alertBox = document.getElementById('errMsg')
+    alertBox.style.display = "block"
+    alertBox.textContent = err
 
-const getCountryData = (num) => {
-    fetch(`https://fakestoreapi.com/products/${num}`)
-        .then(function (response) {
-            console.log(response, "==>> response of fetch")
+    const card = document.getElementsByClassName('card')
+    card[0].style.display = 'none'
+}
+
+
+
+const fetchCountry = (country) => {
+    const findCountry = fetch(`https://restcountries.com/v3.1/name/${country}`)
+
+    findCountry
+        .then((response) => {
+            console.log(response, "response in first then")
+            if (!response.ok) {
+                throw new Error("response is not ok, status text is " + response.statusText)
+            }
             return response.json()
-        }).then(function(data){
-            console.log(data, "==>> my data as json")
-            uiCreation(data)
         })
+        .then(resp => {
+            console.log(resp)
+            uiCreation(resp[0])
+        })
+        .catch((err) => {
+            console.log(err)
+            displayError(err)
+        })
+
 }
 
-getCountryData(1)
+
+fetchCountry('turkey')
 
 
-const getAllProducts = () => {
-    fetch(`https://fakestoreapi.com/products/`)
-        .then(resp => resp.json())
-        .then(data => console.log(data, "==>>allProducts"))
-}
 
-getAllProducts()
+
+const findCountry2 = fetch('https://restcountries.com/v3.1/name/india')
+
+findCountry2
+    // .then(function(response){
+    //     return response.json()
+    // })
+    // .then(function(aslam){
+    //     console.log(aslam)
+    // })
+    .then((response) => response.json())
+    .then((aslam) => console.log(aslam))
