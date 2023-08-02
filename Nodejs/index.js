104d847230da448ef6a99b91d9a799873b8c0af2
innosufiyan <innosufiyan@gmail.com>
2023-08-01 22:32:25 -0700

    server written

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,11 +1,27 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const textIn = fs.readFileSync('./text/input.txt', 'utf-8');
+// synchronous file read
 
-console.log(textIn);
+// const meriFileKaData = fs.readFileSync('./text/input.txt', 'utf-8')
 
-const textOut = `Have you any wool ${Date.now()}`;
+// console.log(meriFileKaData)
 
-fs.writeFileSync('./text/input.txt', textOut);
+// asynchronous file read
 
-console.log('file written');
+fs.readFile('./text/input.txt', 'utf-8', (err, data)=> {
+    if(err) {
+        console.log("phat gaya code")
+    } else {
+        console.log("chal gaya code")
+    }
+})
+
+console.log("checking asynchronous operation")
+
+// synchronous file write
+
+// const meraText = `text changed again n again`
+
+// fs.writeFileSync('./text/input.txt', meraText)
+
+// console.log("file written successfully")
---
+++ Nodejs/server.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const url = require('url')
+
+const port = 8000;
+
+const server = http.createServer((req, res)=> {
+
+
+
+
+    console.log(req.url, "==>> request url")
+    if(req.url === "/") {
+        res.end('this is my home page')
+    } else if (req.url === '/about') {
+        res.end('this is my about page')
+    } else if (req.url === '/contact') {
+        res.end('this is my contact page')
+    } else if (req.url === '/aaloKiPlate') {
+        res.end('aalo ki plate pakrao')
+    } else {
+        res.writeHead(404)
+        res.end('this page is not available, 404 error')
+    }
+})
+
+server.listen(port, '127.0.0.1', ()=> {
+    console.log(`server is working on port number ${port}`)
+})
