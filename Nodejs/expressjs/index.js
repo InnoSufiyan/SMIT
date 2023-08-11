f87084370c95e633fc92d5e5da3ac67d701b5930
innosufiyan <innosufiyan@gmail.com>
2023-08-10 22:20:25 -0700

    CRUD done

--- Nodejs/expressjs/index.js
+++ Nodejs/expressjs/index.js
@@ -4,35 +4,53 @@ const app = express();
 
 const port = 8000;
 
-app.get('/', (req, res)=> {
-    res.status(200).send("Welcome to the home page")
-})
+// app.get('/', (req, res)=> {
+//     res.status(200).send("Welcome to the home page")
+// })
 
-app.get('/about', (req, res)=> {
-    res.status(200).send("Welcome to the about page")
-})
-app.get('/contact', (req, res)=> {
-    res.status(200).send("Welcome to the contact page")
-})
-app.get('/dahiBaray', (req, res)=> {
-    res.status(200).send("yeh lo dahiBaray")
-})
-app.get('/bhelPuri', (req, res)=> {
-    res.status(200).json({
-        plate: 1,
-        name: "bhelPuri",
-        price: 500
-    })
-})
-app.post('/bhelPuri', (req, res)=> {
-    res.status(200).send("Jani post bhel puri ki request ban gai")
-})
-app.delete('/bhelPuri', (req, res)=> {
-    res.status(200).send("Jani bhelPuri delete kardi")
-})
-app.put('/bhelPuri', (req, res)=> {
-    res.status(200).send("Jani bhelPuri main masala daal diya hai")
-})
+// app.get('/about', (req, res)=> {
+//     res.status(200).send("Welcome to the about page")
+// })
+// app.get('/contact', (req, res)=> {
+//     res.status(200).send("Welcome to the contact page")
+// })
+// app.get('/dahiBaray', (req, res)=> {
+//     res.status(200).send("yeh lo dahiBaray")
+// })
+// app.get('/bhelPuri', (req, res)=> {
+//     res.status(200).json({
+//         plate: 1,
+//         name: "bhelPuri",
+//         price: 500
+//     })
+// })
+// app.post('/bhelPuri', (req, res)=> {
+//     res.status(200).send("Jani post bhel puri ki request ban gai")
+// })
+// app.delete('/bhelPuri', (req, res)=> {
+//     res.status(200).send("Jani bhelPuri delete kardi")
+// })
+// app.put('/bhelPuri', (req, res)=> {
+//     res.status(200).send("Jani bhelPuri main masala daal diya hai")
+// })
+
+
+// app.post('/register', (req, res)=> {
+    
+// })
+// app.post('/login', (req, res)=> {
+
+// })
+
+// app.post('/forgetPassword', (req, res)=> {
+
+// })
+
+// app.get('/gettours', (req, res)=> {})
+// app.get('/singleTour/:id', (req, res)=> {})
+
+// app.post('/addTours', (req, res)=>{})
+// app.delete('/deleteTours', (req, res)=>{})
 
 app.listen(port, ()=> {
    console.log(`Sun raha hun server ko port number ${port} per`) 
--- Nodejs/tours/index.js
+++ Nodejs/tours/index.js
@@ -12,6 +12,8 @@ const parsedToursData = JSON.parse(toursJson)
 
 console.log(JSON.parse(toursJson), "==>>toursJson")
 
+app.use(express.json())
+
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: "success",
@@ -37,6 +39,70 @@ app.get('/api/v1/tours/:tourId', (req, res) => {
         data: singleTour
     })
 })
+
+app.post('/api/v1/tours', (req, res) => {
+    console.log(parsedToursData.data.length, "already available tours")
+
+    if (!req.body.destinationName || !req.body.country || !req.body.tourCapacity || !req.body.departure || !req.body.arrival || !req.body.price || !req.body.transport) {
+        res.status(400).send({
+            status: "Rejected",
+            data: "Missing Fields"
+        })
+    }
+
+    const datatoWriteInDb = {
+        id: parsedToursData.data.length + 1,
+        ...req.body
+    }
+
+    parsedToursData.data.push(datatoWriteInDb)
+
+    fs.writeFile('./tours.json', JSON.stringify(parsedToursData), () => {
+        res.status(200).send({
+            status: "Success",
+            data: "Data added Successfully"
+        })
+    })
+})
+
+app.delete('/api/v1/tours/:tourId', (req, res) => {
+    console.log(req.params.tourId)
+    const filteredData = parsedToursData.data.filter(tour => tour.id != (req.params.tourId * 1))
+
+    parsedToursData.data = filteredData
+
+    fs.writeFile('./tours.json', JSON.stringify(parsedToursData), () => {
+        res.status(200).send({
+            status: "success",
+            data: "Successfully Deleted"
+        })
+    })
+})
+
+app.put('/api/v1/tours/:tourId', (req, res) => {
+    console.log(req.params.tourId)
+
+    let indexNumber;
+
+    parsedToursData.data.forEach((tour, idx) => {
+        if (tour.id === (req.params.tourId * 1)) {
+            indexNumber = idx
+        }
+    })
+
+    parsedToursData.data.splice(indexNumber, 1, req.body)
+
+    // parsedToursData.data.splice()
+
+    fs.writeFile('./tours.json', JSON.stringify(parsedToursData), () => {
+        res.status(200).send({
+            status: "success",
+            data: "Update Successfully"
+        })
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port number ${port}`)
